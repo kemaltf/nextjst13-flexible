5640cf05583cef73bbe66f429e38254dd6daf8dc
kemaltf <kemaltaufikf@gmail.com>
2023-10-22 01:13:51 +0700

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-=======
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -41,14 +25,8 @@ module.exports = {
       },
       maxWidth: {
         '10xl': '1680px',
->>>>>>> db21015 (add global tailwind css)
       },
     },
   },
   plugins: [],
-<<<<<<< HEAD
-}
-export default config
-=======
 };
->>>>>>> db21015 (add global tailwind css)
