943f98972d14f9fa5280c9ddf98e5dd76c07507c
kemaltf <kemaltaufikf@gmail.com>
2023-10-23 23:05:33 +0700

    adding navbar and footer in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
 export const metadata = {
   title: 'Flexibble',
   description: 'Showcase and discover remarable developer projects',
@@ -8,9 +11,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        Navbar
+        <Navbar />
         <main>{children}</main>
-        Footer
+        <Footer />
       </body>
     </html>
   );
